7269fb0b249d67c4a6da58bc018c07a5e169664a
Vraj Prajapati <vrajip@gmail.com>
2024-04-06 22:16:52 -0400

    Getting coefficient to relate distance per step

--- trash-code.ino
+++ trash-code.ino
@@ -4,8 +4,9 @@
 
 
 // Stepper Stuff
-const int S_IN4 = 26, S_IN3 = 28, S_IN2 = 32, S_IN1 = 30, stepsPer = 200;
+const int S_IN4 = 26, S_IN3 = 28, S_IN2 = 32, S_IN1 = 12, stepsPer = 200;
 Stepper stepper(stepsPer, S_IN1, S_IN2, S_IN3, S_IN4);
+int travel;
 
 // Compressor Stuff
 const int C_IN3 = 23, C_IN4 = 22, C_enB = 6, S1 = 27, S2 = 29;
@@ -15,8 +16,12 @@ const int IR1 = 33, IR2 = 35;
 
 // Ultrasonic Stuff
 const int ECHO = 25, TRIG = 31;
-NewPing sonar(TRIG, ECHO, 100);
-unsigned long pingTime = millis();
+NewPing sonar(TRIG, ECHO, 50);
+unsigned long pingTime = millis(), dist;
+const int rightBoxCompress = 1323,
+  rightBoxDispense = 555,
+  leftBoxCompress = 1727,
+  leftBoxDispense = 1343;
 
 // Servo stuff
 const int servoPin = 9;
@@ -45,20 +50,28 @@ void setup() {
   //gate.write(180); // Move to closed to begin with
 
   // Stepper
-  stepper.setSpeed(60);
+  stepper.setSpeed(80);
 }
 
 void loop() {
-  // Testing the Stepper
-  delay(50);
-  Serial.print("Dist: ");
-  Serial.println(sonar.ping_cm());
-  /*
+  // Asynchronous Ping-Response Checker
+  /*if (millis() >= pingTime) {
+    pingTime = millis() + 100; // Check every 100 ms
+    dist = sonar.ping_median(10, 50);
+    Serial.println("Dist: ");
+    Serial.println(dist);
+  }*/
+
+  dist = sonar.ping_median(10, 50);
+  delay(100);
   stepper.step(stepsPer);
   delay(500);
+  dist = dist - sonar.ping_median(10, 50);
+  Serial.print("Dist for 200steps: ");
+  Serial.println(dist / 200);
 
   stepper.step(-stepsPer);
   delay(500);
 
-  */
+
 }
